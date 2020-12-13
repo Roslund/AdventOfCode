f5a37aafbacdf5c25ed24b252bfdfd0c0c7846dd
Roslund <enari@minit.nu>
2020-12-13 14:48:59 +0100

    Day10part2

--- 2020/Day10/main.swift
+++ 2020/Day10/main.swift
@@ -1,4 +1,3 @@
-
 let adapters = input.split(separator: "\n").map({Int($0)!}).sorted().reversed()
 
 let chain = [adapters.max()!+3] + adapters + [0]
@@ -8,3 +7,13 @@ let joltdiff = sequence.map { (a,b) in
 }
 
 print(joltdiff.filter({$0 == 3}).count * joltdiff.filter({$0 == 1}).count)
+
+
+var dict = [0:1]
+
+for adapter in chain.dropLast().reversed() {
+    dict[adapter] = dict[adapter-1, default: 0] +
+                    dict[adapter-2, default: 0] +
+                    dict[adapter-3, default: 0]
+}
+print(dict[chain.first!]!)
