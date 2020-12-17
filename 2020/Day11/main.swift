2b5c898e7e3e211a75867f2755a25503a6bef33d
Roslund <enari@minit.nu>
2020-12-17 09:02:22 +0100

    Day16

--- 2020/Day11/main.swift
+++ 2020/Day11/main.swift
@@ -1,36 +1,3 @@
-extension Array where Element: RandomAccessCollection {
-    var rows: Int { self.count}
-    var columns: Int { self.first!.count}
-    
-    subscript(row: Int, column: Int) -> Element.Element? {
-        get {
-            guard row >= 0 && row < self.count else { return nil }
-            guard column >= 0 && column < self[row].count else { return nil }
-            
-            return self[row][column as! Element.Index]
-        }
-    }
-    
-    func elementsAdjacent(to position: (Int, Int)) -> [Element.Element] {
-        let (row, column) = position
-        var neighbours: [Element.Element] = []
-        
-        for i in row-1 ... row+1 {
-            for j in column-1 ... column+1 {
-                // Self should not be includes as a neighbour
-                if i == row && j == column {
-                    continue
-                }
-                
-                if let neighbour = self[i, j] {
-                    neighbours.append(neighbour)
-                }
-            }
-        }
-        return neighbours
-    }
-}
-
 enum Cell: String {
     case seat = "L"
     case occupied = "#"
