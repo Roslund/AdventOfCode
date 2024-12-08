79d7dcdbe971cff7892ecb2ac1877990e92a43e2
Roslund <enari@minit.nu>
2024-12-08 18:41:09 +0100

    Day 12

---
+++ 2022/Day12/main.swift
@@ -0,0 +1,10 @@
+//write a swift program for calculating the shortest path using breadth first search. The map is 2d and represented as a nested array of 1's and 0's.
+
+//This path reaches the goal in 31 steps, the fewest possible.
+let sample = """
+Sabqponm
+abcryxxl
+accszExk
+acctuvwj
+abdefghi
+"""
