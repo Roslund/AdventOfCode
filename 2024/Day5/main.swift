a16680c2cdc2a3b0edba4c1e65c66fb749e06924
Roslund <enari@minit.nu>
2024-12-27 22:53:37 +0100

    Day 5

--- 2024/Day5/main.swift
+++ 2024/Day5/main.swift
@@ -1,50 +1,59 @@
 import Foundation
 
-let srules = """
-47|53
-97|13
-97|61
-97|47
-75|29
-61|13
-75|53
-29|13
-97|29
-53|29
-61|53
-97|53
-61|29
-47|13
-75|47
-97|75
-47|61
-75|61
-47|29
-75|13
-53|13
-"""
-
-let supdates = """
-75,47,61,53,29
-97,61,53,29,13
-75,29,13
-75,97,47,61,53
-61,13,29
-97,13,75,29,47
-"""
-
-//parsning
-
-let rules = srules
+let rules = irules
     .components(separatedBy: .newlines)
     .map({$0.split(separator: "|").map({Int($0)!})})
 
-rules.println()
-
-let updates = supdates
+let updates = iupdates
     .components(separatedBy: .newlines)
     .map({$0.split(separator: ",").map({Int($0)!})})
 
-updates.println()
 
-//Solving
+func middle(_ update: [Int]) -> Int {
+    update[update.count/2]
+}
+
+func validate(_ update: [Int]) -> Bool {
+    rules.map({ rule in
+        if let a = update.firstIndex(of: rule[0]) {
+            if let b = update.firstIndex(of: rule[1]) {
+                return a < b
+            }
+        }
+        return true
+    }).reduce(true, {$0 && $1})
+}
+
+
+updates
+    .map({ validate($0) ? middle($0) : 0})
+    .reduce(0, +)
+    .print(prefix: "Part1")
+
+
+// Part 2
+func fixviolations(_ update: [Int]) -> [Int] {
+    var fixedUpdate = update
+    rules.forEach({ rule in
+        if let a = fixedUpdate.firstIndex(of: rule[0]) {
+            if let b = fixedUpdate.firstIndex(of: rule[1]) {
+                if a > b {
+                    fixedUpdate.insert(fixedUpdate.remove(at: b), at: a)
+                }
+            }
+        }
+    })
+    return fixedUpdate
+}
+
+
+var invalidupdates = updates.filter({!validate($0)})
+
+while !invalidupdates.filter({!validate($0)}).isEmpty {
+    invalidupdates = invalidupdates.map(fixviolations(_:))
+}
+    
+invalidupdates
+    .map(middle(_:))
+    .reduce(0, +)
+    .print(prefix: "Part2")
